f9c5477803cba9538b1645fa8960c6116eaa0c88
Jennifer Hong <hongjenn24@gmail.com>
2021-11-22 19:16:04 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+
+// multiply
+
+let num1 = 62;
+let num2 = 1;
+
+const multiply = num1 * num2;
+
+// random 
+
+const random = Math.ceil(Math.random() * 10) + 1;
+
+
+// mod
+
+let num3 = 16
+let num4 = 4
+
+const mod = num3 / num4
+
+
+// max
+
+const max = Math.max(1, 3, 20)
