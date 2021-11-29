e507ab757c0bef11af10b2f9c44f31230c374331
Jennifer Hong <hongjenn24@gmail.com>
2021-11-29 01:50:28 -0800

    commit tree 15842456acf7f8d2bf0613b49cbfe3bbcb45eba2

--- index.js
+++ index.js
@@ -1,26 +1,70 @@
 
-// Write your code here
+// // Write your code here
 
-// multiply
+// // multiply
 
-let num1 = 62;
-let num2 = 1;
+// let num1 = 62;
+// let num2 = 1;
 
-const multiply = num1 * num2;
+// const multiply = num1 * num2;
 
-// random 
+// // random 
 
-const random = Math.ceil(Math.random() * 10) + 1;
-
-
-// mod
-
-let num3 = 16
-let num4 = 4
-
-const mod = num3 / num4
+// const random = Math.ceil(Math.random() * 10) + 1;
 
 
-// max
+// // mod
 
-const max = Math.max(1, 3, 20)
+// let num3 = 16
+// let num4 = 4
+
+// const mod = num3 / num4
+
+
+// // max
+
+// const max = Math.max(1, 3, 20)
+
+// let num1 = 1;
+// let num2 = 62;
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+}
+
+multiply(1, 62);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
